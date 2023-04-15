8eaa866fe0863654f3ebb456ed77392a45bc6a6a
Yurikash <ur_serg@yahoo.com>
2023-04-15 11:41:57 -0400

    class 10 2D Arrays

---
+++ src/class10Arrays2D/E5D2Arrays.java
@@ -0,0 +1,21 @@
+package class10Arrays2D;
+
+public class E5D2Arrays {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {10, 20, 30, 40, 50},
+                {20, 50, 65, 20},
+                {102, 54, 60},
+                {20,55}
+        };
+        for (int row = 0; row < matrix.length; row++) {
+            for (int colomn = 0; colomn <matrix[row].length ; colomn++) {
+                System.out.print(matrix[row][colomn]+" ");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
